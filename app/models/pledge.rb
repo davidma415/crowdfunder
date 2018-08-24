807b45d90eb057cf703dc5a6a27b599f2becd393
David Ma <davidma415@gmail.com>
2018-08-24 13:48:12 -0400

    Finished validations for models

--- app/models/pledge.rb
+++ app/models/pledge.rb
@@ -4,4 +4,10 @@ class Pledge < ApplicationRecord
 
   validates :dollar_amount, presence: true
   validates :user, presence: true
+
+  validate :check_owner
+
+  def check_owner
+    errors.add(:user_id, "cannot contribute to their own project") if project.user == self.user
+  end
 end
--- app/models/reward.rb
+++ app/models/reward.rb
@@ -1,3 +1,4 @@
 class Reward < ActiveRecord::Base
+  validates :description, :dollar_amount, presence: true
   belongs_to :project
 end
