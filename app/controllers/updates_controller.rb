c1a4f4cce597dd3644534a39dcc728d23fb65b55
David Ma <davidma415@gmail.com>
2018-08-29 01:44:03 -0400

    Finished adding errors to empty update, added validations

--- app/controllers/updates_controller.rb
+++ app/controllers/updates_controller.rb
@@ -3,29 +3,34 @@ class UpdatesController < ApplicationController
 
   def create
     @project = Project.find(params[:project_id])
+    @backers = @project.users
+    @latest_updates = @project.latest_updates(5)
     @update = Update.new
     @update.name = params[:update][:name]
     @update.description = params[:update][:description]
     @update.project_id = params[:project_id]
     @update.user_id = current_user.id
     if @update.save
-      flash.now[:alert] = ["Successfully added update"]
-      redirect_to root_url
+      flash.now[:alert] = "Successfully added update"
+      redirect_to project_url(@project)
     else
-      flash.now[:alert] = ["Failed to make an update."]
-      render projects_path
+      flash.now[:alert] = "Failed to make an update."
+      render 'projects/show'
     end
   end
+
   def edit
     @update = Update.find(params[:id])
     @project = Project.find(params[:project_id])
   end
+
   def update
   end
+
   def destroy
     @update = Update.find(params[:id])
     @update.destroy
     flash[:notice] = "You've successfully deleted an update"
-    redirect_to "/projects"
+    redirect_to root_url
   end
 end
--- app/models/update.rb
+++ app/models/update.rb
@@ -1,4 +1,6 @@
 class Update < ApplicationRecord
   belongs_to :project
   belongs_to :user
+
+  validates :name, :description, presence: true
 end
